d2cbaaaa4b9159e6ed72105a66cad5030015b3df
Mathijs Vanhaverbeke <mvhave7@micsd01.uz.kuleuven.ac.be>
2024-02-18 14:00:45 +0100

    Push everything to branch

---
+++ GrappaUNet/UsingPreprocessedData/PreprocessingCode/preprocess_test_data.py
@@ -0,0 +1,107 @@
+import h5py
+import numpy as np
+from pathlib import Path
+from fastmri.data import transforms as T
+from pygrappa import grappa
+import torch
+import time
+import gc
+
+folder_path = '/usr/local/micapollo01/MIC/DATA/SHARED/NYU_FastMRI/Preprocessed/multicoil_test/'
+files = Path(folder_path).glob('**/*')
+file_count = 1
+
+def to_tensor(data: np.ndarray) -> torch.Tensor:
+    """
+    Convert numpy array to PyTorch tensor.
+
+    For complex arrays, the real and imaginary parts are stacked along the last
+    dimension.
+
+    Args:
+        data: Input numpy array.
+
+    Returns:
+        PyTorch version of data.
+    """
+    if np.iscomplexobj(data):
+        data = np.stack((data.real, data.imag), axis=-1)
+
+    return torch.from_numpy(data)
+
+def tensor_to_complex_np(data: torch.Tensor) -> np.ndarray:
+    """
+    Converts a complex torch tensor to numpy array.
+
+    Args:
+        data: Input data to be converted to numpy.
+
+    Returns:
+        Complex numpy version of data.
+    """
+    return torch.view_as_complex(data).numpy()
+
+def apply_grappa(masked_kspace, mask):
+    """
+    Applies GRAPPA algorithm
+    References
+    ----------
+    [1] Griswold, Mark A., et al. "Generalized autocalibrating
+       partially parallel acquisitions (GRAPPA)." Magnetic
+       Resonance in Medicine: An Official Journal of the
+       International Society for Magnetic Resonance in Medicine
+       47.6 (2002): 1202-1210.
+    Args:
+        masked_kspace (torch.Tensor): Multi-coil masked input k-space of shape (num_coils, rows, cols, 2)
+        mask (torch.Tensor): Applied mask of shape (1, 1, cols, 1)
+    Returns:
+        preprocessed_masked_kspace (torch.Tensor): Output of GRAPPA algorithm applied on masked_kspace
+    """
+
+    def get_low_frequency_lines(mask):
+        l = r = mask.shape[-2] // 2
+        while mask[..., r, :]:
+            r += 1
+
+        while mask[..., l, :]:
+            l -= 1
+
+        return l + 1, r
+
+    l, r = get_low_frequency_lines(mask)
+    num_low_freqs = r - l
+    pad = (mask.shape[-2] - num_low_freqs + 1) // 2
+    calib = masked_kspace[:, :, pad:pad + num_low_freqs].clone()
+    preprocessed_masked_kspace = grappa(tensor_to_complex_np(masked_kspace), tensor_to_complex_np(calib), kernel_size=(5, 5), coil_axis=0)
+    return to_tensor(preprocessed_masked_kspace)
+
+for file in files:
+    print(str(file_count)+". Starting to process file "+str(file)+'...')
+    hf = h5py.File(file, 'a') # Open in append mode
+    kspace = hf['kspace'][()] # kspace is already masked here
+    print("Shape of the raw kspace: ", str(np.shape(kspace)))
+    kspace_torch = T.to_tensor(kspace)
+    print("Shape of the torch tensor: ", str(kspace_torch.shape))
+    mask = hf['mask'][()]
+    mask = np.expand_dims(mask, axis=-1)
+    mask = np.expand_dims(mask, axis=0)
+    mask = np.expand_dims(mask, axis=0)
+    mask = np.expand_dims(mask, axis=0)
+    mask = T.to_tensor(mask)
+    print("Shape of the test mask: ", str(mask.shape))
+    grappa_data = torch.zeros([kspace_torch.shape[0],kspace_torch.shape[1],kspace_torch.shape[2],kspace_torch.shape[3],kspace_torch.shape[4]])
+    for slice in range(kspace_torch.shape[0]):
+        grappa_data[slice,:,:,:,:] = apply_grappa(kspace_torch[slice,:,:,:,:], mask[0,:,:,:,:])
+    print("Shape of the preprocessed grappa data: ", str(grappa_data.shape))
+    grappa_data = tensor_to_complex_np(grappa_data)
+    print("Shape of the numpy-converted grappa data: ", str(grappa_data.shape))
+    # Add a key to the h5 file with grappa_data inside it
+    hf.create_dataset('grappa_data', data=grappa_data)
+    hf.close()
+    time.sleep(1)
+    del kspace, kspace_torch, mask, grappa_data
+    time.sleep(1)
+    gc.collect()
+    file_count += 1
+    print('Done.')
+
---
+++ GrappaUNet/UsingPreprocessedData/preprocessed_transforms.py
@@ -0,0 +1,294 @@
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from typing import Dict, NamedTuple, Optional, Tuple, Union
+
+import numpy as np
+import torch
+
+import fastmri
+
+from fastmri.data.subsample import MaskFunc
+
+
+def to_tensor(data: np.ndarray) -> torch.Tensor:
+    """
+    Convert numpy array to PyTorch tensor.
+
+    For complex arrays, the real and imaginary parts are stacked along the last
+    dimension.
+
+    Args:
+        data: Input numpy array.
+
+    Returns:
+        PyTorch version of data.
+    """
+    if np.iscomplexobj(data):
+        data = np.stack((data.real, data.imag), axis=-1)
+
+    return torch.from_numpy(data)
+
+
+def tensor_to_complex_np(data: torch.Tensor) -> np.ndarray:
+    """
+    Converts a complex torch tensor to numpy array.
+
+    Args:
+        data: Input data to be converted to numpy.
+
+    Returns:
+        Complex numpy version of data.
+    """
+    return torch.view_as_complex(data).numpy()
+
+
+def center_crop(data: torch.Tensor, shape: Tuple[int, int]) -> torch.Tensor:
+    """
+    Apply a center crop to the input real image or batch of real images.
+
+    Args:
+        data: The input tensor to be center cropped. It should
+            have at least 2 dimensions and the cropping is applied along the
+            last two dimensions.
+        shape: The output shape. The shape should be smaller
+            than the corresponding dimensions of data.
+
+    Returns:
+        The center cropped image.
+    """
+    if not (0 < shape[0] <= data.shape[-2] and 0 < shape[1] <= data.shape[-1]):
+        raise ValueError("Invalid shapes.")
+
+    w_from = (data.shape[-2] - shape[0]) // 2
+    h_from = (data.shape[-1] - shape[1]) // 2
+    w_to = w_from + shape[0]
+    h_to = h_from + shape[1]
+
+    return data[..., w_from:w_to, h_from:h_to]
+
+
+def complex_center_crop(data: torch.Tensor, shape: Tuple[int, int]) -> torch.Tensor:
+    """
+    Apply a center crop to the input image or batch of complex images.
+
+    Args:
+        data: The complex input tensor to be center cropped. It should have at
+            least 3 dimensions and the cropping is applied along dimensions -3
+            and -2 and the last dimensions should have a size of 2.
+        shape: The output shape. The shape should be smaller than the
+            corresponding dimensions of data.
+
+    Returns:
+        The center cropped image
+    """
+    if not (0 < shape[0] <= data.shape[-3] and 0 < shape[1] <= data.shape[-2]):
+        raise ValueError("Invalid shapes.")
+
+    w_from = (data.shape[-3] - shape[0]) // 2
+    h_from = (data.shape[-2] - shape[1]) // 2
+    w_to = w_from + shape[0]
+    h_to = h_from + shape[1]
+
+    return data[..., w_from:w_to, h_from:h_to, :]
+
+
+def center_crop_to_smallest(
+    x: torch.Tensor, y: torch.Tensor
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Apply a center crop on the larger image to the size of the smaller.
+
+    The minimum is taken over dim=-1 and dim=-2. If x is smaller than y at
+    dim=-1 and y is smaller than x at dim=-2, then the returned dimension will
+    be a mixture of the two.
+
+    Args:
+        x: The first image.
+        y: The second image.
+
+    Returns:
+        tuple of tensors x and y, each cropped to the minimim size.
+    """
+    smallest_width = min(x.shape[-1], y.shape[-1])
+    smallest_height = min(x.shape[-2], y.shape[-2])
+    x = center_crop(x, (smallest_height, smallest_width))
+    y = center_crop(y, (smallest_height, smallest_width))
+
+    return x, y
+
+
+def normalize(
+    data: torch.Tensor,
+    mean: Union[float, torch.Tensor],
+    stddev: Union[float, torch.Tensor],
+    eps: Union[float, torch.Tensor] = 0.0,
+) -> torch.Tensor:
+    """
+    Normalize the given tensor.
+
+    Applies the formula (data - mean) / (stddev + eps).
+
+    Args:
+        data: Input data to be normalized.
+        mean: Mean value.
+        stddev: Standard deviation.
+        eps: Added to stddev to prevent dividing by zero.
+
+    Returns:
+        Normalized tensor.
+    """
+    return (data - mean) / (stddev + eps)
+
+
+def normalize_instance(
+    data: torch.Tensor, eps: Union[float, torch.Tensor] = 0.0
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Normalize the given tensor  with instance norm/
+
+    Applies the formula (data - mean) / (stddev + eps), where mean and stddev
+    are computed from the data itself.
+
+    Args:
+        data: Input data to be normalized
+        eps: Added to stddev to prevent dividing by zero.
+
+    Returns:
+        torch.Tensor: Normalized tensor
+    """
+    mean = data.mean()
+    std = data.std()
+
+    return normalize(data, mean, std, eps), mean, std
+
+
+class UnetSample(NamedTuple):
+    """
+    A subsampled image for U-Net reconstruction.
+
+    Args:
+        image: Subsampled image after inverse FFT.
+        target: The target image (if applicable).
+        mean: Per-channel mean values used for normalization.
+        std: Per-channel standard deviations used for normalization.
+        fname: File name.
+        slice_num: The slice index.
+        max_value: Maximum image value.
+    """
+
+    image: torch.Tensor
+    target: torch.Tensor
+    mean: torch.Tensor
+    std: torch.Tensor
+    fname: str
+    slice_num: int
+    max_value: float
+
+
+class UnetDataTransform:
+    """
+    Data Transformer for training U-Net models.
+    """
+
+    def __init__(
+        self,
+        which_challenge: str,
+        mask_func: Optional[MaskFunc] = None,
+        use_seed: bool = True,
+    ):
+        """
+        Args:
+            which_challenge: Challenge from ("singlecoil", "multicoil").
+            mask_func: Optional; A function that can create a mask of
+                appropriate shape.
+            use_seed: If true, this class computes a pseudo random number
+                generator seed from the filename. This ensures that the same
+                mask is used for all the slices of a given volume every time.
+        """
+        if which_challenge not in ("singlecoil", "multicoil"):
+            raise ValueError("Challenge should either be 'singlecoil' or 'multicoil'")
+
+        self.mask_func = mask_func
+        self.which_challenge = which_challenge
+        self.use_seed = use_seed
+
+    ####################################################################################
+    # Modified to be compatible with preprocessed grappa data appended to the h5 files #
+    ####################################################################################
+    def __call__(
+        self,
+        grappa_data: np.ndarray,
+        target: np.ndarray,
+        attrs: Dict,
+        fname: str,
+        slice_num: int,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, str, int, float]:
+        """
+        Args:
+            grappa_data: Input k-space of shape (num_coils, rows, cols) for
+                multi-coil data or (rows, cols) for single coil data.
+            target: Target image.
+            attrs: Acquisition related information stored in the HDF5 object.
+            fname: File name.
+            slice_num: Serial number of the slice.
+
+        Returns:
+            A tuple containing, zero-filled input image, the reconstruction
+            target, the mean used for normalization, the standard deviations
+            used for normalization, the filename, and the slice number.
+        """
+        kspace_torch = to_tensor(grappa_data)
+
+        # check for max value
+        max_value = attrs["max"] if "max" in attrs.keys() else 0.0
+
+        # inverse Fourier transform to get zero filled solution
+        image = fastmri.ifft2c(kspace_torch)
+
+        # crop input to correct size
+        if target is not None:
+            crop_size = (target.shape[-2], target.shape[-1])
+        else:
+            crop_size = (attrs["recon_size"][0], attrs["recon_size"][1])
+
+        # check for FLAIR 203
+        if image.shape[-2] < crop_size[1]:
+            crop_size = (image.shape[-2], image.shape[-2])
+
+        image = complex_center_crop(image, crop_size)
+
+        # absolute value
+        image = fastmri.complex_abs(image)
+
+        # apply Root-Sum-of-Squares if multicoil data
+        if self.which_challenge == "multicoil":
+            image = fastmri.rss(image)
+
+        # normalize input
+        image, mean, std = normalize_instance(image, eps=1e-11)
+        image = image.clamp(-6, 6)
+
+        # normalize target
+        if target is not None:
+            target_torch = to_tensor(target)
+            target_torch = center_crop(target_torch, crop_size)
+            target_torch = normalize(target_torch, mean, std, eps=1e-11)
+            target_torch = target_torch.clamp(-6, 6)
+        else:
+            target_torch = torch.Tensor([0])
+
+        return UnetSample(
+            image=image,
+            target=target_torch,
+            mean=mean,
+            std=std,
+            fname=fname,
+            slice_num=slice_num,
+            max_value=max_value,
+        )
+
